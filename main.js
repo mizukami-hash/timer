8e43b9213678156d664718ab4ddd411a00481744
mizukami-hash <128657673+mizukami-hash@users.noreply.github.com>
2023-06-19 11:42:33 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,83 +1,214 @@
 "use strict";
-
 {
-  const timer = document.querySelector("#timer");
-  const btn = document.querySelector("#btn");
-  const stopBtn=document.querySelector('#stopBtn');
+// 再帰関数調べる
+    const timer = document.querySelector('#timer');
+    const min =document.querySelector('#min');
+    const sec= document.querySelector('#sec');
+    const reset= document.querySelector('#reset');
+    const start = document.querySelector('#start');
 
-  let endTime;
-  let countdown=0;
-  let intervalId;
-  
-
-  function check() {
-    // ３分ー経過時間（現在時間）
-    countdown = endTime - new Date().getTime();
-
-    // ０以下で動作を停止
-    if (countdown < 0) {
-      clearInterval(intervalId);
-      countdown = 3 * 60000; /*3分*/
+    let startTime;
+    let timeToCountDown=0;
+    let timerId;
+    let isRunning =false;
+    let timeLeft;
+    
+    function upDateTimer(t){
+        let d =new Date(t);
+        let m = d.getMinutes();
+        let s = d.getSeconds();
+        let ms = d.getMilliseconds();
+        m =('0'+ m ).slice(-2);
+        s =('0'+ s ).slice(-2);
+        ms =('00'+ ms ).slice(-3);
+        timer.textContent=m+':'+s+':'+ms;
     }
+
+function countDown(){
+    // 残り時間を表示する
+    timerId=setTimeout(() => {
+        timeLeft=  timeToCountDown - (Date.now()-startTime);
+        countDown();
+        upDateTimer(timeLeft);
+
+        if(timeLeft<0){
+            isRunning=false;
+            start.textContent='Start';
+            clearTimeout(timerId);
+            timeLeft=0;
+            timeToCountDown=0;
+            upDateTimer(timeLeft);
+            return;
+        }
+    }, 10);
+}
+    start.addEventListener('click',()=>{
+
+        if(isRunning === false){
+            isRunning =true;
+            start.textContent='Stop';
+            startTime=Date.now();
+            countDown();
+        }else{
+            clearTimeout(timerId);
+            isRunning=false;
+            start.textContent='Start';
+            timeToCountDown=timeLeft;
+
+        }
+    })
+
+    min.addEventListener('click',()=>{
+        if (isRunning=== true){
+            return;
+        }
+        timeToCountDown += 60*1000;
+        if(timeToCountDown < 60){
+            timeToCountDown=0;
+        }
+        upDateTimer(timeToCountDown);
+    })
+
+    sec.addEventListener('click',()=>{
+        if (isRunning=== true){
+            return;
+        }
+        timeToCountDown += 1000;
+        if(timeToCountDown < 60){
+            timeToCountDown=0;
+        }
+        upDateTimer(timeToCountDown);
+    })
+
+    reset.addEventListener('click',()=>{
+        timeToCountDown=0;
+        upDateTimer(timeToCountDown);
+
+    })
+
+
+
+
   
 
-    //   ミリ秒を秒に変換
-    const totalSeconds = Math.floor(countdown / 1000);
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = Math.floor(totalSeconds % 60);
-
-    // ２桁表示
-    const minutesFormatted = String(minutes).padStart(2, "0");
-    const secondsFormatted = String(seconds).padStart(2, "0");
-
-    timer.textContent = `${minutesFormatted}:${secondsFormatted}`;
-  } /*check 閉じタグ*/
 
 
-  btn.addEventListener("click", () => {
-    endTime = new Date().getTime() + 3 * 60000 
-    intervalId = setInterval(check, 100);
-    
-  }); 
-
-//   一時停止する関数
-  function stop(){
-    clearInterval(intervalId);
-    
-     }
-//   再開する関数
-
-  stopBtn.addEventListener('click',()=>{
-    stop();
-
-  })
-
-  // ２個目：練習　今日の残り時間のタイマー
-  // 参考　https://tcd-theme.com/2021/08/javascript-countdowntimer.html
-
-  const hour = document.getElementById("hour");
-  const min = document.getElementById("min");
-  const sec = document.getElementById("sec");
-
-  // 今日の日付＋１で明日の日付を取得し、差分で今日の残り時間を見る
-  function today() {
-    const now = new Date();
-    const tomorrow = new Date(
-      now.getFullYear(),
-      now.getMonth(),
-      now.getDate() + 1
-    );
-    const difference = tomorrow.getTime() - now.getTime();
-
-    const seconds2 = Math.floor(difference / 1000) % 60;
-    const minutes2 = Math.floor(difference / 1000 / 60) % 60;
-    const hour2 = Math.floor(difference / 1000 / 60 / 60);
-
-    hour.textContent = hour2;
-    min.textContent = minutes2;
-    sec.textContent = seconds2;
-  }
-  today();
-  setInterval(today, 1000);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 現在日時を取得
+// スタート時間を設定
+// Passed(elapsed) = now-starttime
+// timetocountdown =5s
+// timetocountdown -elapsedtime =lefttime(表示時間)
+
+//(表示時間) Left = start -passed
+
+//     // 分、秒に変換する関数
+//     function upDateTimer(t){
+//         let d =new Date(t);
+//         let m = d.getMinutes();
+//         let s = d.getSeconds();
+//         let ms = d.getMilliseconds();
+//         m =('0'+ m ).slice(-2);
+//         s =('0'+ s ).slice(-2);
+//         ms =('00'+ ms ).slice(-3);
+//         timer.textContent=m+':'+s+':'+ms;
+//     }
+
+// function countDown(){
+//     // 残り時間を表示する
+//     timerId=setTimeout(() => {
+//         timeLeft=  timeToCountDown - (Date.now()-startTime);
+//         countDown();
+//         upDateTimer(timeLeft);
+
+//         if(timeLeft<0){
+//             isRunning=false;
+//             start.textContent='Start';
+//             clearTimeout(timerId);
+//             timeLeft=0;
+//             timeToCountDown=0;
+//             upDateTimer(timeLeft);
+//             return;
+//         }
+//     }, 10);
+// }
+//     start.addEventListener('click',()=>{
+
+//         if(isRunning === false){
+//             isRunning =true;
+//             start.textContent='Stop';
+//             startTime=Date.now();
+//             countDown();
+//         }else{
+//             clearTimeout(timerId);
+//             isRunning=false;
+//             start.textContent='Start';
+//             timeToCountDown=timeLeft;
+
+//         }
+//     })
+
+//     min.addEventListener('click',()=>{
+//         if (isRunning=== true){
+//             return;
+//         }
+//         timeToCountDown += 60*1000;
+//         if(timeToCountDown < 60){
+//             timeToCountDown=0;
+//         }
+//         upDateTimer(timeToCountDown);
+//     })
+
+//     sec.addEventListener('click',()=>{
+//         if (isRunning=== true){
+//             return;
+//         }
+//         timeToCountDown += 1000;
+//         if(timeToCountDown < 60){
+//             timeToCountDown=0;
+//         }
+//         upDateTimer(timeToCountDown);
+//     })
+
+//     reset.addEventListener('click',()=>{
+//         timeToCountDown=0;
+//         upDateTimer(timeToCountDown);
+
+//     })
+
+// }
