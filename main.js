e15629e6c9fc1e3cdcc964000e0e725e73349c0e
mizukami-hash <128657673+mizukami-hash@users.noreply.github.com>
2023-06-23 21:33:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,137 +1,97 @@
 "use strict";
 {
+  const timer = document.querySelector("#timer");
+  const min = document.querySelector("#min");
+  const sec = document.querySelector("#sec");
+  const reset = document.querySelector("#reset");
+  const start = document.querySelector("#start");
 
-    const timer = document.querySelector('#timer');
-    const min =document.querySelector('#min');
-    const sec= document.querySelector('#sec');
-    const reset= document.querySelector('#reset');
-    const start = document.querySelector('#start');
+  let startTime;
+  let timeToCountDown = 0;
+  let timerId;
+  let isRunning = false;
+  let timeLeft;
 
-  
+  function upDateTimer(t) {
+    let d = new Date(t);
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let ms = d.getMilliseconds();
+    m = ("0" + m).slice(-2);
+    s = ("0" + s).slice(-2);
+    ms = ("00" + ms).slice(-3);
+    timer.textContent = m + ":" + s + "." + ms;
+  }
 
-    let startTime;
-    let timeToCountDown=0;
-    let timerId;
-    let isRunning =false;
-    let timeLeft;
-    
-    function upDateTimer(t){
-        let d =new Date(t);
-        let m = d.getMinutes();
-        let s = d.getSeconds();
-        let ms = d.getMilliseconds();
-        m =('0'+ m ).slice(-2);
-        s =('0'+ s ).slice(-2);
-        ms =('00'+ ms ).slice(-3);
-        timer.textContent=m+':'+s+'.'+ms;
-    }
-
-function countDown(){
+  function countDown() {
     // 残り時間を表示する
-    timerId=setTimeout(() => {
-        timeLeft=  timeToCountDown - (Date.now()-startTime);
-        countDown();
+    timerId = setTimeout(() => {
+      timeLeft = timeToCountDown - (Date.now() - startTime);
+      countDown();
+      upDateTimer(timeLeft);
+
+      if (timeLeft < 0) {
+        isRunning = false;
+        start.textContent = "Start";
+        clearTimeout(timerId);
+        timeLeft = 0;
+        timeToCountDown = 0;
         upDateTimer(timeLeft);
-
-        if(timeLeft<0){
-            isRunning=false;
-            start.textContent='Start';
-            clearTimeout(timerId);
-            timeLeft=0;
-            timeToCountDown=0;
-            upDateTimer(timeLeft);
-            return;
+        return;
+      }
+      if (timeLeft < 180000) {
+        timer.classList.add("three-minutes");
+        if (timeLeft < 30000) {
+          timer.classList.add("thirty-seconds");
+          if (timeLeft < 10) {
+            timer.classList.remove("thirty-seconds");
+            timer.classList.remove("three-minutes");
+          }
         }
-        if(timeLeft < 180000){
-            timer.classList.add("three");
-         if(timeLeft < 30000){
-            timer.classList.add("thirty");
-            if(timeLeft<10){
-                timer.classList.remove("thirty");
-                timer.classList.remove("three");
-            }
-        }
-        }
+      }
     }, 10);
+  }
+  start.addEventListener("click", () => {
+    if (isRunning === false) {
+      isRunning = true;
+      start.textContent = "Stop";
+      startTime = Date.now();
+      countDown();
+    } else {
+      clearTimeout(timerId);
+      isRunning = false;
+      start.textContent = "Start";
+      timeToCountDown = timeLeft;
+    }
+  });
+
+  min.addEventListener("click", () => {
+    if (isRunning === true) {
+      return;
+    }
+    timeToCountDown += 60 * 1000;
+    if (timeToCountDown < 60) {
+      timeToCountDown = 0;
+    }
+    upDateTimer(timeToCountDown);
+  });
+
+  sec.addEventListener("click", () => {
+    if (isRunning === true) {
+      return;
+    }
+    timeToCountDown += 1000;
+    if (timeToCountDown < 60) {
+      timeToCountDown = 0;
+    }
+    upDateTimer(timeToCountDown);
+  });
+
+  reset.addEventListener("click", () => {
+    timeToCountDown = 0;
+    upDateTimer(timeToCountDown);
+  });
 }
-    start.addEventListener('click',()=>{
-
-        if(isRunning === false){
-            isRunning =true;
-            start.textContent='Stop';
-            startTime=Date.now();
-            countDown();
-        }else{
-            clearTimeout(timerId);
-            isRunning=false;
-            start.textContent='Start';
-            timeToCountDown=timeLeft;
-
-        }
-    })
-
-    min.addEventListener('click',()=>{
-        if (isRunning=== true){
-            return;
-        }
-        timeToCountDown += 60*1000;
-        if(timeToCountDown < 60){
-            timeToCountDown=0;
-        }
-        upDateTimer(timeToCountDown);
-    })
-
-    sec.addEventListener('click',()=>{
-        if (isRunning=== true){
-            return;
-        }
-        timeToCountDown += 1000;
-        if(timeToCountDown < 60){
-            timeToCountDown=0;
-        }
-        upDateTimer(timeToCountDown);
-    })
-
-    reset.addEventListener('click',()=>{
-        timeToCountDown=0;
-        upDateTimer(timeToCountDown);
-
-    })
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // 現在日時を取得
 // スタート時間を設定
